8a978137ad589308b905c166edd851993693db6f
eele94 <eele_mailadres@hotmail.com>
2023-11-30 13:52:08 +0100

    feat: migrations

--- database/migrations/create_laravel-tonic_offers_table.php
+++ database/migrations/create_laravel-tonic_offers_table.php
@@ -3,14 +3,16 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Eele94\Tonic\Models\TonicVertical;
 
-return new class extends Migration
+return new class() extends Migration
 {
     public function up()
     {
-        Schema::create('verticals', function (Blueprint $table) {
+        Schema::create('tonic_offers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignIdFor(TonicVertical::class, 'vertical_id')->nullable()->constrained('tonic_verticals', 'id');
             $table->timestamps();
         });
     }
--- database/migrations/create_laravel-tonic_verticals_table.php
+++ database/migrations/create_laravel-tonic_verticals_table.php
@@ -3,16 +3,14 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Eele94\Tonic\Models\TonicVertical;
 
 return new class extends Migration
 {
     public function up()
     {
-        Schema::create('offers', function (Blueprint $table) {
+        Schema::create('tonic_verticals', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignIdFor(TonicVertical::class, 'vertical_id')->nullable()->constrained();
             $table->timestamps();
         });
     }
